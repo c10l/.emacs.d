6f2c840751030bc0237a7b991ff67f277eb54357
Cassiano Leal <cassianoleal@gmail.com>
2016-09-19 23:30:15 +0100

    Move PATH to mac init file

--- init/init-defaults.el
+++ init/init-defaults.el
@@ -26,18 +26,6 @@
 
 ; (global-flycheck-mode)
 
-;; Set PATH from shell
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (replace-regexp-in-string
-                          "[ \t\n]*$"
-                          ""
-                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq eshell-path-env path-from-shell) ; for eshell users
-    (setq exec-path (split-string path-from-shell path-separator))))
-
-(when window-system (set-exec-path-from-shell-PATH))
-
 (desktop-save-mode 1)
 
 (provide 'init-defaults)
