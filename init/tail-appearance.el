4bdb316eecb0e155adf6c534c0cfd80f01265b5e
Cassiano Leal <cassianoleal@gmail.com>
2016-09-20 21:01:47 +0100

    Appearance tweaks -- line numbers, theme, beacon

--- init/tail-appearance.el
+++ init/tail-appearance.el
@@ -1,8 +1,25 @@
-(use-package darkokai-theme :ensure)
-; (load-theme 'darkokai t)
-(global-linum-mode)
+(use-package darkokai-theme
+  :ensure
+  :config
+  (progn
+    (load-theme 'darkokai t)))
 
-(use-package beacon :ensure)
-(beacon-mode 1)
+(use-package nlinum
+  :ensure
+  :config
+  (progn
+    ;; Preset `nlinum-format' for minimum width.
+    (defun my-nlinum-mode-hook ()
+      (when nlinum-mode
+        (setq-local nlinum-format
+                    (setq linum-format "%3d \u2502"))))
+    (global-nlinum-mode 1)
+    (add-hook 'nlinum-mode-hook 'my-nlinum-mode-hook)))
+
+(use-package beacon
+  :ensure
+  :config
+  (progn
+    (beacon-mode 1)))
 
 (provide 'tail-appearance)
