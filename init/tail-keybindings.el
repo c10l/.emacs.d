4f4cf3489aa0dc37383570f29a74516a99775171
Cassiano Leal <cassianoleal@gmail.com>
2016-10-11 14:18:01 +0100

    Add keybinding for multiple cursors in region

--- init/tail-keybindings.el
+++ init/tail-keybindings.el
@@ -14,5 +14,6 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-l") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 
 (provide 'tail-keybindings)
