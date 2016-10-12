29fdc39c055eb6090ea5b8b3c2a2a7af03ff3782
Cassiano Leal <cassianoleal@gmail.com>
2016-10-12 16:42:18 +0100

    Enable helm-find-files

--- init/tail-keybindings.el
+++ init/tail-keybindings.el
@@ -5,7 +5,7 @@
 
 ;; helm
 (global-set-key (kbd "M-x") 'helm-M-x)
-;(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 ;; File navigation
 (global-set-key (kbd "C-x C-b") 'ibuffer-other-window)
