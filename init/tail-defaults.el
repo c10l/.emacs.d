4741667143dc25f0ffbdc3ed92cc79cce9f01cb2
Cassiano Leal <cassianoleal@gmail.com>
2016-10-17 16:11:10 +0100

    Save backups to ~/.emacs.d

--- init/tail-defaults.el
+++ init/tail-defaults.el
@@ -38,4 +38,12 @@
    kept-old-versions 2
    version-control t)       ; use versioned backups
 
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
+
 (provide 'tail-defaults)
