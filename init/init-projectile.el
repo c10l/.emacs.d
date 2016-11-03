078d883b432f261d634a7aefa8b0e8979ccb58be
Cassiano Leal <cassianoleal@gmail.com>
2016-11-03 13:59:21 +0000

    Fix projectile, add Neotree

--- init/init-projectile.el
+++ init/init-projectile.el
@@ -1,14 +1,24 @@
 (use-package projectile
+  :commands (all-the-icons-icon-for-dir all-the-icons-icon-for-file)
   :ensure
   :config
   (progn
     (use-package helm-projectile :ensure)
-    (autoload
-      'projectile-mode
-      "projectile-mode"
-      "Project Management"
-      t)
+    (use-package neotree :ensure)
+    (projectile-mode 1)
     (helm-projectile-on)
+    (defun neotree-project-dir ()
+      "Open NeoTree using the git root."
+      (interactive)
+      (let ((project-dir (projectile-project-root))
+            (file-name (buffer-file-name)))
+        (if project-dir
+            (if (neotree-toggle)
+                (progn
+                  (neotree-dir project-dir)
+                  (neotree-find file-name)))
+          (message "Could not find git project root."))))
+    (global-set-key [f8] 'neotree-project-dir)
     ))
 
 (provide 'init-projectile)
